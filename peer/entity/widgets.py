ea71ab667eba252d42546b5f8978566fbc2ee39f
Emilio Jiménez <jimenezmoral@gmail.com>
2015-07-22 01:29:49 +0200

    changes to join both editors

--- peer/entity/widgets.py
+++ peer/entity/widgets.py
@@ -3,14 +3,12 @@ from django import forms
 from django.utils.safestring import SafeData, SafeText, mark_safe
 
 class MetadataWidget(forms.widgets.Textarea):
-    def __init__(self, attrs=None):
-        attrs = attrs or {}
-        md_attrs = {'id': 'xmlcontent',
-                   'placeholder': 'The xml elements'}
-        attrs.update(md_attrs)
-        super(MetadataWidget, self).__init__(attrs)  
+    # def __init__(self, attrs=None):
+    #     attrs = attrs or {}
+    #     md_attrs = {'hidden': ''}
+    #     attrs.update(md_attrs)
+    #     super(MetadataWidget, self).__init__(attrs)  
 
     def render(self, name, value, attrs=None):
         html = super(MetadataWidget, self).render(name, value, attrs)
-        return mark_safe('<div id="samlmetaeditor"></div>')
-
+        return mark_safe('<div id="samlmetaeditor"></div>' + html)
