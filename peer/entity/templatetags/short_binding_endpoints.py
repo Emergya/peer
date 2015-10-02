9fd57805ac4faa615a89f27a025e5f8777d3f506
albluqmun <albluqmun@gmail.com>
2015-10-02 11:14:16 +0200

    4 columns no icons

---
+++ peer/entity/templatetags/short_binding_endpoints.py
@@ -0,0 +1,11 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+
+@register.filter
+@stringfilter
+def get_binding_short(string):
+        splited_string = string.split(':')
+        return splited_string[-1]
